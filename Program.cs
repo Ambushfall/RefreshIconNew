f2a40514c36e0a23a9492d8ef979450027cb7c76
Ambushfall <lukathetormentor@gmail.com>
2024-01-13 04:51:09 +0100

    git init

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RefreshIcon
+{
+    internal partial class Program
+    {
+        [System.Runtime.InteropServices.LibraryImport("Shell32.dll")]
+        private static partial int SHChangeNotify(int eventId, int flags, IntPtr item1, IntPtr item2);
+
+        static void Main()
+        {
+            SHChangeNotify(0x8000000, 0x0000, IntPtr.Zero, IntPtr.Zero);
+        }
+    }
+}
